ea8e4749a6d27f4d87eedf36f0e96c42945a89bf
Sauvageot <Otomaton010@gmail.com>
2020-10-21 09:41:50 +0200

    Player controller
    added basic movements

---
+++ Assets/Script/Playerbehaviour.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Playerbehaviour : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    [SerializeField] private float maxSpeed;
+    private Inputs inputs;
+    private Vector2 direction;
+
+    private void OnEnable()
+    {
+        inputs = new Inputs();
+        inputs.Enable();
+        inputs.player.move.performed += OnMovePerformed;
+        inputs.player.move.canceled += OnMoveCanceled;
+    }
+
+    private void OnMoveCanceled(InputAction.CallbackContext obj)
+    {
+        direction = Vector2.zero;
+    }
+
+    private void OnMovePerformed(InputAction.CallbackContext obj)
+    {
+        direction = obj.ReadValue<Vector2>();
+        Debug.Log(direction);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+        var myRigidBody = GetComponent<Rigidbody2D>();
+        direction.y = 0;
+        //myRigidBody.velocity = direction;
+        if (myRigidBody.velocity.sqrMagnitude < maxSpeed)
+        myRigidBody.AddForce(direction * speed);
+    }
+}
